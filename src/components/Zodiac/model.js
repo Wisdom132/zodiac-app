30fab444954a1d7aa9fa658cbda92f4265b120c9
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-28 00:58:29 +0100

    clean up db

--- src/components/Zodiac/model.js
+++ src/components/Zodiac/model.js
@@ -1,65 +1,53 @@
 export default [{
-        name: "Capricórnio",
-        start: new Date("2017/01/01"),
-        end: new Date("2017/01/20"),
-        img: "/imgs/capricornio"
-    } {
-        name: "Aquário",
-        start: new Date("2017/01/21"),
-        end: new Date("2017/02/19"),
-        img: "/imgs/aquario"
-    }, {
-        name: "Peixes",
-        start: new Date("2017/02/20"),
-        end: new Date("2017/03/20"),
-        img: "/imgs/peixes"
-    }, {
-        name: "Áries",
-        start: new Date("2017/03/21"),
-        end: new Date("2017/04/20"),
-        img: "/imgs/aries"
-    }, {
-        name: "Touro",
-        start: new Date("2017/04/21"),
-        end: new Date("2017/05/21"),
-        img: "/imgs/touro"
-    }, {
-        name: "Gêmeos",
-        start: new Date("2017/05/22"),
-        end: new Date("2017/06/20"),
-        img: "/imgs/gemeos"
-    }, {
-        name: "Câncer",
-        start: new Date("2017/06/21"),
-        end: new Date("2017/07/23"),
-        img: "/imgs/cancer"
-    }, {
-        name: "Leão",
-        start: new Date("2017/07/24"),
-        end: new Date("2017/08/23"),
-        img: "/imgs/leao"
-    }, {
-        name: "Virgem",
-        start: new Date("2017/08/24"),
-        end: new Date("2017/09/23"),
-        img: "/imgs/virgem"
-    }, {
-        name: "Libra",
-        start: new Date("2017/09/24"),
-        end: new Date("2017/10/23"),
-        img: "/imgs/libra"
-    }, {
-        name: "Escorpião",
-        start: new Date("2017/10/24"),
-        end: new Date("2017/11/22"),
-        img: "/imgs/escorpiao"
-    }, {
-        name: "Sagitário",
-        start: new Date("2017/11/23"),
-        end: new Date("2017/12/21"),
-        img: "/imgs/sargitario"
-    }, {
-        name: "Capricórnio",
-        start: new Date("2017/12/22"),
-        end: new Date("2017/12/31"),
-        img: "/imgs/capricornio"]
+    name: "Capricórnio",
+    start: new Date("2017/01/01"),
+    end: new Date("2017/01/20")
+}, {
+    name: "Aquário",
+    start: new Date("2017/01/21"),
+    end: new Date("2017/02/19"),
+}, {
+    name: "Peixes",
+    start: new Date("2017/02/20"),
+    end: new Date("2017/03/20"),
+}, {
+    name: "Áries",
+    start: new Date("2017/03/21"),
+    end: new Date("2017/04/20"),
+}, {
+    name: "Touro",
+    start: new Date("2017/04/21"),
+    end: new Date("2017/05/21"),
+}, {
+    name: "Gêmeos",
+    start: new Date("2017/05/22"),
+    end: new Date("2017/06/20"),
+}, {
+    name: "Câncer",
+    start: new Date("2017/06/21"),
+    end: new Date("2017/07/23"),
+}, {
+    name: "Leão",
+    start: new Date("2017/07/24"),
+    end: new Date("2017/08/23"),
+}, {
+    name: "Virgem",
+    start: new Date("2017/08/24"),
+    end: new Date("2017/09/23"),
+}, {
+    name: "Libra",
+    start: new Date("2017/09/24"),
+    end: new Date("2017/10/23"),
+}, {
+    name: "Escorpião",
+    start: new Date("2017/10/24"),
+    end: new Date("2017/11/22"),
+}, {
+    name: "Sagitário",
+    start: new Date("2017/11/23"),
+    end: new Date("2017/12/21"),
+}, {
+    name: "Capricórnio",
+    start: new Date("2017/12/22"),
+    end: new Date("2017/12/31"),
+}]
