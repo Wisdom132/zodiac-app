5d61eefc80efa24d202fc9c8b83f4bbd6e65de4b
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-27 22:34:04 +0100

    binded card height and width

--- src/components/Zodiac/ZodiacView.vue
+++ src/components/Zodiac/ZodiacView.vue
@@ -1,24 +1,23 @@
 <template>
   <section class="d-flex">
-    <v-card class="mx-auto h-50 w-100 bg-grey-2 mt-5" width="344">
+    <v-card class="mx-auto w-100 bg-grey-2 mt-5" :height="height" :width="width">
       <v-card-text>
         <div>Word of the Day</div>
         <p class="display-1 text--primary">be•nev•o•lent</p>
-        <p>adjective</p>
-        <div class="text--primary">
-          well meaning and kindly.
-          <br />"a benevolent smile"
-        </div>
       </v-card-text>
-      <v-card-actions>
-        <v-btn text color="deep-purple accent-4">Learn More</v-btn>
-      </v-card-actions>
     </v-card>
 
     <div class="map-sidebar elevation-3 d-flex h-100">
       <div class="tabs brl br-grey-4 pa-5">
         <div>
           <label class="overline">Customize your design</label>
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-select :items="items" label="Change Size" solo></v-select>
+              </v-col>
+            </v-row>
+          </v-container>
         </div>
       </div>
 
@@ -30,17 +29,18 @@
 </template>
 
 <script>
-// import zodiacView from "@/components/Zodiac/ZodiacView";
 import sidebar from "./sidebar";
 export default {
   data() {
     return {
-      active: true
+      active: true,
+      height: 300,
+      width: 300,
+      items: ["Foo", "Bar", "Fizz", "Buzz"]
     };
   },
   components: {
     sidebar
-    // zodiacView
   }
 };
 </script>
