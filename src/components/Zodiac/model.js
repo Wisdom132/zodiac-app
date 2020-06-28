36f79cd0c13f6c521251ea17ea32ff4778a81ee7
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-28 02:38:49 +0100

    defined sign dataset

--- src/components/Zodiac/model.js
+++ src/components/Zodiac/model.js
@@ -1,53 +1,89 @@
 export default [{
-    name: "Capricórnio",
-    start: new Date("2017/01/01"),
-    end: new Date("2017/01/20")
-}, {
+
     name: "Aquário",
+    element: "Air",
+    tag: "PROGRESSIVE • IMAGINATIVE • INDEPENDENT",
+    subline: "JAN 20 - FEB 18",
     start: new Date("2017/01/21"),
     end: new Date("2017/02/19"),
 }, {
     name: "Peixes",
+
+    element: "WATER",
+    tag: "COMPASSIONATE • INTUITIVE • ARTISTIC",
+    subline: "FEB 19 - MAR 20",
+
     start: new Date("2017/02/20"),
     end: new Date("2017/03/20"),
 }, {
-    name: "Áries",
+    name: "Aries",
+    element: "FIRE",
+    tag: "ENTHUSIASTIC • DYNAMIC • HONEST",
+    subline: "MAR 21 - APR 19",
+
     start: new Date("2017/03/21"),
     end: new Date("2017/04/20"),
 }, {
-    name: "Touro",
+    name: "TAURUS",
+    element: "EARTH",
+    tag: "PATIENT • DEPENDABLE • DEVOTED",
+    subline: "APR 20 - MAY 20",
     start: new Date("2017/04/21"),
     end: new Date("2017/05/21"),
 }, {
-    name: "Gêmeos",
+    name: "GEMINI",
+    element: "AIR",
+    tag: "EXPRESSIVE • CURIOUS • WITTY",
+    subline: "MAY 21 - JUN 21",
     start: new Date("2017/05/22"),
     end: new Date("2017/06/20"),
 }, {
-    name: "Câncer",
+    name: "CANCER",
+    element: "WATER",
+    tag: "LOYAL • SENTIMENTAL • COMPASSIONATE",
+    subline: "JUN 22 - JUL 22",
     start: new Date("2017/06/21"),
     end: new Date("2017/07/23"),
 }, {
-    name: "Leão",
+    name: "LEO",
+    element: "FIRE",
+    tag: "ENERGETIC • OUTGOING • SELF-ASSURED",
+    subline: "JUL 23 - AUG 22",
     start: new Date("2017/07/24"),
     end: new Date("2017/08/23"),
 }, {
-    name: "Virgem",
+    name: "VIRGO",
+    element: "EARTH",
+    tag: "HARDWORKING • LOYAL • TRUSTWORTHY",
+    subline: "AUG 23 - SEP 22",
     start: new Date("2017/08/24"),
     end: new Date("2017/09/23"),
 }, {
-    name: "Libra",
+    name: "LIBRA",
+    element: "AIR",
+    tag: "CHARMING • DIPLOMATIC • GRACIOUS",
+    subline: "SEP 23 - OCT 23",
     start: new Date("2017/09/24"),
     end: new Date("2017/10/23"),
 }, {
-    name: "Escorpião",
+    name: "SCORPIO",
+    element: "WATER",
+    tag: "PASSIONATE • RESOURCEFUL • BRAVE",
+    subline: "OCT 24 - NOV 21",
     start: new Date("2017/10/24"),
     end: new Date("2017/11/22"),
 }, {
-    name: "Sagitário",
+    name: "SAGITTARIUS",
+    element: "FIRE",
+    tag: "OPTIMISTIC • FUNNY • GENEROUS",
+    subline: "NOV 22 - DEC 21",
     start: new Date("2017/11/23"),
     end: new Date("2017/12/21"),
 }, {
-    name: "Capricórnio",
+    name: "CAPRICORN",
+    element: "EARTH",
+    tag: "RESPONSIBLE • AMBITIOUS • WISE",
+    subline: "DEC 22 - JAN 19",
     start: new Date("2017/12/22"),
     end: new Date("2017/12/31"),
 }]
