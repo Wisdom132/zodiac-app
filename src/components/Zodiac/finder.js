e8fe0613af78db8db51371f61b7fda62a5427c00
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-28 03:36:18 +0100

    clean up services

--- src/components/Zodiac/finder.js
+++ src/components/Zodiac/finder.js
@@ -1,13 +1,4 @@
 import Zodiacs from './model.js'
-
-// export default function (date) {
-//     return Zodiacs.find((zodiac) => {
-//         if (zodiac.start <= date && zodiac.end >= date) {
-//             return zodiac
-//         }
-//     })
-// }
-
 export let getByDate = date => {
     return Zodiacs.find((zodiac) => {
         if (zodiac.start <= date && zodiac.end >= date) {
